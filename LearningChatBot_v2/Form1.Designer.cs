0a5fe69aea3c816d2668f1ecea67360e1a026d17
UnknownRussian <No@mail.com>
2018-03-12 01:05:58 +0100

    Project Added
    First upload

---
+++ LearningChatBot_v2/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace LearningChatBot_v2
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxDialog = new System.Windows.Forms.TextBox();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.userInput = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBoxDialog
+            // 
+            this.textBoxDialog.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxDialog.Location = new System.Drawing.Point(12, 12);
+            this.textBoxDialog.Multiline = true;
+            this.textBoxDialog.Name = "textBoxDialog";
+            this.textBoxDialog.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxDialog.Size = new System.Drawing.Size(271, 150);
+            this.textBoxDialog.TabIndex = 0;
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonSend.Location = new System.Drawing.Point(208, 168);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(75, 23);
+            this.buttonSend.TabIndex = 1;
+            this.buttonSend.Text = "Send";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // userInput
+            // 
+            this.userInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.userInput.Location = new System.Drawing.Point(12, 170);
+            this.userInput.Name = "userInput";
+            this.userInput.Size = new System.Drawing.Size(190, 20);
+            this.userInput.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(295, 200);
+            this.Controls.Add(this.userInput);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxDialog);
+            this.Name = "MainForm";
+            this.Text = "LearningChatBot_v2";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxDialog;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox userInput;
+    }
+}
+
---
+++ LearningChatBot_v2/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace LearningChatBot_v2
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MyData info = new MyData();
+                info.Data1 = userInput.Text;
+                info.Data2 = "Hello";
+                info.Data3 = "World";
+                SaveData(info, "test.xml");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public static void SaveData(object obj, string filename)
+        {
+            XmlSerializer sr = new XmlSerializer(obj.GetType());
+            TextWriter writer = new StreamWriter(filename);
+            sr.Serialize(writer, obj);
+            writer.Close();            
+        }
+
+        public void MainBrain()
+        {
+
+        }
+    }
+
+    public class MyData
+    {
+        private string data1;
+        private string data2;
+        private string data3;
+
+        public string Data1
+        {
+            get { return data1; }
+            set { data1 = value; }
+        }
+        public string Data2
+        {
+            get { return data2; }
+            set { data2 = value; }
+        }
+        public string Data3
+        {
+            get { return data3; }
+            set { data3 = value; }
+        }
+    }
+}
+
+
